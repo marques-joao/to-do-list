58e94d1b1c112823f3e0524061c817b3b7c2f135
João Marques <jvitormarques79@gmail.com>
2024-06-19 23:10:54 -0300

    build(todolist): adiciona dependências para o backend

---
+++ backend/src/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => response.status(200).send('Olá, mundo!'))
+
+app.listen(3333, () => console.log('Server running on port 3333'))
