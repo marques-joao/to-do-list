7ac88308bc53e15dcfb26c1e70abf6b6142f6e66
João Marques <jvitormarques79@gmail.com>
2024-06-24 19:31:53 -0300

    feat(todolist): cria função que formata data para o padrão Brasil

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -24,6 +24,14 @@ const addTask = async (event) => {
     inputTask.value = '';
 }
 
+const formatDate = (dateUTC) => {
+    const options = { dateStyle: 'long', timeStyle: 'short' };
+
+    const date = new Date(dateUTC).toLocaleString('pt-br', options);
+
+    return date;
+}
+
 const createElement = (tag, innerText = '', innerHTML = '') => {
     const element = document.createElement(tag);
 
@@ -58,7 +66,7 @@ const createRow = (task) => {
     const tr = createElement('tr');
     
     const tdTitle = createElement('td', title);
-    const tdCreatedAt = createElement('td', created_at);
+    const tdCreatedAt = createElement('td', formatDate(created_at));
     const tdStatus = createElement('td');
     const tdActions = createElement('td');
 
