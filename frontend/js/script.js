055bf81f9ca21d47caef8753d740ec1e49deac80
João Marques <jvitormarques79@gmail.com>
2024-06-24 18:40:25 -0300

    feat(todolist): cria função para gerar elementos na tela utilizando task genérica

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,5 @@
+const tbody = document.querySelector('tbody');
+
 const fetchTasks = async () => {
     const response = await fetch('http://localhost:3333/tasks');
     const tasks = await response.json();
@@ -5,4 +7,52 @@ const fetchTasks = async () => {
     return tasks;
 }
 
-fetchTasks();
+const createElement = (tag, innerText = '', innerHTML = '') => {
+    const element = document.createElement(tag);
+
+    if (innerText) {
+        element.innerText = innerText;
+    }
+    
+    if (innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+
+    return element;
+}
+
+const task = {
+    id: 1,
+    title: 'Teste',
+    created_at: '01 de janeiro de 2024 23:04',
+    status: 'pendente'
+}
+
+const createRow = (task) => {
+    const { id, title, created_at, status } = task;
+
+    const tr = createElement('tr');
+    
+    const tdTitle = createElement('td', title);
+    const tdCreatedAt = createElement('td', created_at);
+    const tdStatus = createElement('td');
+    const tdActions = createElement('td');
+
+    const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
+    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
+
+    editButton.classList.add('btn-action');
+    deleteButton.classList.add('btn-action');
+
+    tdActions.appendChild(editButton);
+    tdActions.appendChild(deleteButton);
+
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdCreatedAt);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdActions);
+
+    tbody.appendChild(tr);
+}
+
+createRow(task);
